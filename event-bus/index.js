06aae213cdeaaec03e81b791a86c92559f2425f6
jimmy2952 <jimmy2952@gmail.com>
2022-01-18 20:39:22 +0800

    complete k8s config

--- event-bus/index.js
+++ event-bus/index.js
@@ -11,10 +11,10 @@ app.post('/events', (req, res) => {
 
   events.push(event);
   try {    
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
-    axios.post('http://localhost:4003/events', event);
+    axios.post('http://posts-clusterip-srv:4000/events', event);
+    // axios.post('http://localhost:4001/events', event);
+    // axios.post('http://localhost:4002/events', event);
+    // axios.post('http://localhost:4003/events', event);
   } catch (err) {
     console.log(err);
   }
